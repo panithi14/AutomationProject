cca2feb8b45a94f1a97656b8376a651f8043909c
baby <sampanithi89@gmail.com>
2023-01-09 17:26:09 +0530

    f1 is added

---
+++ src/main/java/com/qa/opencart/pages/Automation.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class Automation {
+
+	private By register = By.id("register");
+
+	public void registrationpage() {
+		System.out.println("registration page");
+	}
+}
---
+++ src/main/java/com/qa/opencart/pages/Feature.java
@@ -0,0 +1,11 @@
+package com.qa.opencart.pages;
+
+public class Feature {
+	
+	public static void main (String a[]) {
+		
+		
+		System.out.println("f1");
+	}
+
+}
