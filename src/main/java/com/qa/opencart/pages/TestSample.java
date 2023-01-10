3f24607d65c2a308e3b6d30d7a49301eeedf1315
panithi14 <122170857+panithi14@users.noreply.github.com>
2023-01-10 09:04:56 +0530

    Create TestSample.java

---
+++ src/main/java/com/qa/opencart/pages/TestSample.java
@@ -0,0 +1,13 @@
+package com.qa.opencart.pages;
+
+public class TestSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Test Demo");
+
+
+	}
+
+}
